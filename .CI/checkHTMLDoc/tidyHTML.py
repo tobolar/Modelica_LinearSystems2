114c33992591bb02fb86800e8720a6179990478d
tobolar <tobolar@users.noreply.github.com>
2026-05-12 10:36:29 +0200

    Git bump actions (#271)
    * Bump actions/checkout and actions/setup-python
    * Update copyright year

--- .CI/checkHTMLDoc/tidyHTML.py
+++ .CI/checkHTMLDoc/tidyHTML.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 '''
-Copyright (C) 2020-2024, Modelica Association and contributors
+Copyright (C) 2020-2026, Modelica Association and contributors
 All rights reserved.
 
 Check Modelica HTML documentation with HTML Tidy
